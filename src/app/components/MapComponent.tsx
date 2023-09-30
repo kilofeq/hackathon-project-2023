e7bde4f308f2d2cd1af550120bbeaadd05097f85
Arek Jarnecki <arkadiusz@jarnecki.pl>
2023-09-30 19:27:45 +0200

    map conflicts

--- src/app/components/MapComponent.tsx
+++ src/app/components/MapComponent.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-<<<<<<< Updated upstream:src/app/components/MapComponent.tsx
-import GoogleMapReact from "google-map-react";
-import { useEffect, useState } from "react";
-=======
 import GoogleMap from 'google-maps-react-markers'
 import {useEffect, useState} from "react";
 import {InfoMarker} from "@/assets/infoMarker";
->>>>>>> Stashed changes:src/app/components/MapComponent/MapComponent.tsx
+
 
 export const MapComponent = () => {
 
   const [userLocalization, setUserLocalization] = useState({lat: 0, lng: 0})
-
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((loc) => {
       setUserLocalization({lat: loc.coords.latitude ,lng: loc.coords.longitude})
