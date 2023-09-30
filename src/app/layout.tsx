26ebcc2c30bc68dbffe10db65aee99feab21260b
Kacper Barcik <45320966+kilofeq@users.noreply.github.com>
2023-09-30 21:07:32 +0200

    Merge branch 'main' into origin/frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] });
 
